5b9727c004716f783be151a52b45e949c9b73d2e
Petras Slekys <petras@ibm.com>
2022-05-16 21:09:03 +0300

    added file upload functionality

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const usersRoute = require('./routes/v1/users');
 const petsRoute = require('./routes/v1/pets');
 const medsRoute = require('./routes/v1/medications');
+const docsRoute = require('./routes/v1/documents');
 const { port } = require('./config');
 
 const app = express();
@@ -14,6 +15,7 @@ app.get('/', (req, res) => res.send({ msg: 'Server is running' }));
 app.use('/v1/users/', usersRoute);
 app.use('/v1/pets/', petsRoute);
 app.use('/v1/medications/', medsRoute);
+app.use('/v1/docs/', docsRoute);
 
 app.all('*', (req, res) => res.status(404).send({ err: 'Page not found' }));
 
---
+++ src/routes/v1/documents.js
@@ -0,0 +1,67 @@
+/* eslint-disable prefer-template */
+/* eslint-disable no-path-concat */
+const express = require('express');
+const fs = require('fs');
+const mysql = require('mysql2/promise');
+const multer = require('multer');
+const loggedIn = require('../../middleware/auth');
+const { mySqlConfig } = require('../../config');
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, './docs'),
+  filename: (req, file, cb) => cb(null, `${Math.floor(Math.random() * 1000)}.pdf`),
+});
+
+const upload = multer({ storage });
+
+router.get('/', loggedIn, async (req, res) => {
+  try {
+    const con = await mysql.createConnection(mySqlConfig);
+    const [data] = await con.execute(`SELECT * FROM documents WHERE user_id = ${req.user.id}`);
+    await con.end();
+
+    return res.send(data);
+  } catch (err) {
+    return res.status(500).send({ msg: 'something wrong with server, please try again later' });
+  }
+});
+
+router.get('/:filename', loggedIn, async (req, res) => {
+  try {
+    const con = await mysql.createConnection(mySqlConfig);
+    const [data] = await con.execute(`SELECT * FROM documents WHERE filename = ${mysql.escape(req.params.filename)}`);
+    await con.end();
+
+    if (req.user.id === data[0].user_id) {
+      const file = fs.readFileSync(__dirname + `/../../../docs/${req.params.filename}`);
+      return res.end(file);
+    }
+
+    return res.status(401).send({ err: 'Unauthorized' });
+  } catch (err) {
+    return res.status(500).send({ msg: 'something wrong with server, please try again later' });
+  }
+});
+
+router.post('/', loggedIn, upload.single('document'), async (req, res) => {
+  try {
+    const con = await mysql.createConnection(mySqlConfig);
+    const [data] = await con.execute(`
+        INSERT INTO documents (filename, user_id)
+        VALUES ('${req.file.filename}', ${req.user.id})
+    `);
+    await con.end();
+
+    if (!data.insertId) {
+      return res.status(500).send({ msg: 'something wrong with server, please try again later' });
+    }
+
+    return res.send({ msg: 'Successfully added a document' });
+  } catch (err) {
+    return res.status(500).send({ msg: 'something wrong with server, please try again later' });
+  }
+});
+
+module.exports = router;
